5f5a88e94c0833f3deb237d2de659b44fa73f521
이경용 <sleekydz86@naver.com>
2024-02-14 23:10:27 +0900

    front basic

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,8 +1,12 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-	content: [],
+/** @type {import('tailwindcss').Config}*/
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
 	theme: {
 		extend: {}
 	},
-	plugins: []
+
+	plugins: [require('daisyui')]
 };
+
+module.exports = config;
