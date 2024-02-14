d0ecf6aafba77ec76d96b815abe7d2929c95b7d7
이경용 <sleekydz86@naver.com>
2024-02-14 21:35:16 +0900

    Api ,Member 컨트롤 반영

--- back/src/main/java/com/todostudy/myjob/domain/member/controller/ApiV1MemberController.java
+++ back/src/main/java/com/todostudy/myjob/domain/member/controller/ApiV1MemberController.java
@@ -1,4 +1,67 @@
 package com.todostudy.myjob.domain.member.controller;
 
+import com.todostudy.myjob.domain.member.dto.MemberDto;
+import com.todostudy.myjob.domain.member.record.LoginRequestBody;
+import com.todostudy.myjob.domain.member.record.LoginResponseBody;
+import com.todostudy.myjob.domain.member.record.MeResponseBody;
+import com.todostudy.myjob.domain.member.service.MemberService;
+import com.todostudy.myjob.global.https.ReqData;
+import com.todostudy.myjob.global.https.RespData;
+import com.todostudy.myjob.standard.base.Empty;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import static org.springframework.http.MediaType.ALL_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+@RestController
+@RequestMapping(value = "/api/v1/members", consumes  = APPLICATION_JSON_VALUE,produces = APPLICATION_JSON_VALUE)
+@Tag(name="ApiV1MemberController",description="회원 CRUD 컨트롤러")
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ApiV1MemberController {
+
+    private final MemberService memberService;
+    private final ReqData req;
+
+    @PostMapping(value="/login")
+    @Operation(summary = "로그인, accessToken, refreshToken 쿠키 생성됨")
+    public RespData<LoginResponseBody> login(@Valid @RequestBody LoginRequestBody body){
+        RespData<MemberService.AuthAndMakeTokensResponseBody> authAndMakeTokensRs = memberService.authAndMakeTokens(
+                body.username,
+                body.password
+        );
+
+        req.setCrossDomainCookie("refreshToken",authAndMakeTokensRs.getData().refreshToken());
+        req.setCrossDomainCookie("accessToken",authAndMakeTokensRs.getData().accessToken());
+
+        return authAndMakeTokensRs.newDataOf(
+                new LoginResponseBody(
+                        new MemberDto(authAndMakeTokensRs.getData().member())
+                )
+        );
+    }
+
+    @GetMapping(value ="/me",consumes = ALL_VALUE)
+    @Operation(summary = "내 정보")
+    @SecurityRequirement(name = "bearrerAuth")
+    public RespData<MeResponseBody> getMe(){
+        return RespData.of(
+                new MeResponseBody(
+                        new MemberDto(req.getMember())
+                )
+        );
+    }
+
+    @PostMapping(value = "/logout", consumes = ALL_VALUE)
+    @Operation(summary = "로그아웃")
+    public RespData<Empty> logout(){
+        req.setLogout();
+
+        return RespData.of("로그아웃 성공");
+    }
 }
--- back/src/main/java/com/todostudy/myjob/domain/member/controller/MemberController.java
+++ back/src/main/java/com/todostudy/myjob/domain/member/controller/MemberController.java
@@ -1,4 +1,54 @@
 package com.todostudy.myjob.domain.member.controller;
 
+
+import com.todostudy.myjob.global.https.ReqData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/member")
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Tag(name ="MemberController",description="스웨거 로그인 용 로그인 폼, 카카오 로그인 기능 다양한 기능 제공")
 public class MemberController {
+
+    private final ReqData req;
+
+    @GetMapping("/login")
+    @ResponseBody
+    @Operation(summary = "관리자 페이지에서 스웨거 로그인시에 ADMIN권한을 필요로함, 그것을 위한 로그인 폼 보여줌")
+    public String login(){
+        return """
+                 <form method="POST">
+                                    <div>
+                                        <label>아이디</label>
+                                        <input type="text" name="username">
+                                    </div>
+                                    <div>
+                                        <label>비밀번호</label>
+                                        <input type="password" name="password">
+                                    </div>
+                                    <div>
+                                        <button type="submit">로그인</button>
+                                    </div>
+                                </form>
+                """;
+    }
+
+    @GetMapping("/socialLogin/{providerTypeCode}")
+    @Operation(summary = "프론트에서 소셜 로그인 후 원래 페이지로 돌아가기 위한 용도로 사용됨, redirectUrl 파라미터를 받아서 쿠키에 저장함")
+    public String socialLogin(String redirectUrl, @PathVariable String prividerTypeCode){
+        if(req.isFrontUrl(redirectUrl)){
+            req.setCookie("redirectUrlAfterSocialLogin",redirectUrl,60*10);
+        }
+        return "redirect:/oauth2/authorization/"+prividerTypeCode;
+    }
+
 }
