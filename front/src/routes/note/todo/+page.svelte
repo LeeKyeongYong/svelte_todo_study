d6b88a073efd9afadc0b690463ebbfabe3910937
이경용 <sleekydz86@naver.com>
2024-02-15 15:35:31 +0900

    모달 기능 추가

--- front/src/routes/note/todo/+page.svelte
+++ front/src/routes/note/todo/+page.svelte
@@ -6,6 +6,8 @@
 		completed: boolean;
 	}
 
+	let editingTodo: Todo = $state({ id: 0, content: '', completed: false });
+
 	function makeTodo(id: number, content: string, completed: boolean) {
 		return { id, content, completed };
 	}
@@ -25,16 +27,33 @@
 		form.todoContent.value = '';
 		form.todoContent.focus();
 	}
+
+	function showTodoEditModal(todo: Todo) {
+		Object.assign(editingTodo, todo);
+		const modal = document.getElementById('todo-edit-modal') as HTMLDialogElement;
+		modal.showModal();
+		modal.querySelector('input')?.focus();
+	}
+	function submitEditTodoForm(this: HTMLFormElement, event: Event) {
+		const form = this;
+		todos = todos.map((todo) =>
+			todo.id == editingTodo.id ? { ...todo, content: editingTodo.content } : todo
+		);
+		const modal = document.getElementById('todo-edit-modal') as HTMLDialogElement;
+		modal.close();
+	}
 </script>
 
 {#snippet TodoList(todos)}
 
-<h2>할일 목록</h2>
+<h2>할일</h2>
 <ul>
 	{#each todos as todo (todo.id)}
 		<li>
 			{todo.content}
 			<input type="checkbox" class="checkbox" bind:checked={todo.completed} />
+			<button class="btn btn-outline btn-sm" onclick={() => showTodoEditModal(todo)}>수정</button>
+			<button class="btn btn-outline btn-sm">삭제</button>
 		</li>
 	{/each}
 </ul>
@@ -51,6 +70,43 @@
 	{/each}
 </ul>
 {/snippet}
+
+<dialog id="todo-edit-modal" class="modal">
+	<div class="modal-box">
+		<form method="dialog">
+			<button class="btn btn-circle btn-ghost btn-sm absolute right-2 top-2">✕</button>
+		</form>
+		<form class="grid gap-3" on:submit|preventDefault={submitEditTodoForm}>
+			<label class="form-control">
+				<div class="label">
+					<span class="label-text">할일</span>
+				</div>
+				<input
+					type="text"
+					placeholder="할일을 입력해주세요."
+					class="input input-bordered"
+					bind:value={editingTodo.content}
+				/>
+			</label>
+			<div class="grid grid-cols-2 gap-3">
+				<button class="btn btn-primary">저장</button>
+				<button
+					type="button"
+					class="btn btn-outline"
+					onclick={() => {
+					const modal = document.getElementById('todo-edit-modal') as HTMLDialogElement;
+					modal.close();
+				}}
+					>취소</button
+				>
+			</div>
+		</form>
+	</div>
+
+	<form method="dialog" class="modal-backdrop">
+		<button></button>
+	</form>
+</dialog>
 
 {@render TodoList(todos)}
 <h3/>
